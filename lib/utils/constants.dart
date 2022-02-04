170af3143bff89733456a270c5f3afd09fca7bf1
Airat Galyamov <galyamov.airat@gmail.com>
2022-02-05 00:41:55 +0500

    Обновления

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,5 +1,5 @@
 const String URL_GET_USERS_LIST = 'https://jsonplaceholder.typicode.com/users';
 const String URL_GET_TODOS_BY_USER =
     'https://jsonplaceholder.typicode.com/todos?userId=';
-const String userLogin = 'user@gmail.com';
-const String userPass = '12345';
+const String userLogin = 'user@main.ru';
+const String userPass = '111';
